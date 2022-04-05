273591d83f313285cca6ef8480068fea2b4cfb8f
GABRIEL OLIVEIRA <gabriel.oliveira@euax.com.br>
2022-04-05 13:51:40 -0300

    Adicionando javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function teste() {
+    
+    const name = document.getElementById('name').value 
+    const mail = document.getElementById('mail').value
+    const area = document.getElementById('area').value
+    
+    return alert(`Woooooow \n \n Seja bem-vindo ${name} ao bootcamp da área de ${area}. \n \n Você se cadastrou com o e-mail ${mail}.`)
+}
